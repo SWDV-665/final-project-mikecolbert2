5fef185766a1b95d80fcf0fb77b7980dc71fb461
Mike Colbert <mcolbert1@live.maryville.edu>
2020-12-08 09:22:40 -0600

    rudimentary interface

--- habits/src/pages/home/home.ts
+++ habits/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,63 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
 
+  title = "My Habit Stacker"
+
+  //items array
+  habits = [
+    {
+      'name' : 'testing'
+    }
+  ];
+
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+  }
+
+  didToday(habit){
+    const done = Date();   
+    console.log("I did this today...", done, habit);
+  }
+
+  removeHabit(habit){
+    console.log("I deleted this...", habit);
+  }
+
+  addHabit(){
+    console.log("adding item");
+    this.showAddHabitPrompt();
+    //console.log(this.habits)
+  }
+
+  showAddHabitPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter item ... ",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'What daily habit are you working on?'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: habit => {
+            console.log('Saved clicked', habit);
+            this.habits.push(habit); //push this item from the login into our items array
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
--- habits/src/pages/streak/streak.ts
+++ habits/src/pages/streak/streak.ts
@@ -7,6 +7,28 @@ import { NavController } from 'ionic-angular';
 })
 export class StreakPage {
 
+  title = "My Habit Stacker"
+
+  //items array
+  items = [
+    {
+      date: "day 1",
+      habit: "test1"
+    },
+    {
+      date: "day 2",
+      habit: "test2"
+    },
+    {
+      date: "day 3",
+      habit: "test3"
+    },
+    {
+      date: "day 4",
+      habit: "test4"
+    },
+  ];
+
   constructor(public navCtrl: NavController) {
 
   }
